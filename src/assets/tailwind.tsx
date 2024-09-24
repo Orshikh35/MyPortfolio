b2eef191a379084648855910ec15e66dbdc52b88
Bat Orshikh <batorshikh@Bats-MacBook-Pro.local>
2024-09-24 16:54:18 +0800

    dsc

--- src/assets/tailwind.tsx
+++
@@ -1,8 +0,0 @@
-import tailwind from "../assets/download-removebg-preview (1).png"
-export default function TW() {
-    return(
-        <div>
-               <img src={tailwind.src} alt=""  className="w-[69px]"/>
-        </div>
-    )
-}
